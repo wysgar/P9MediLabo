93d1a02437dfa9af445afbb683164a921423d452
Mickael <jametmickael1@gmail.com>
2025-04-25 00:00:22 +0200

    Ajout Dockerfile Patient

--- patient/src/main/java/com/patient/api/model/Patient.java
+++ patient/src/main/java/com/patient/api/model/Patient.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 @Entity
 public class Patient {
@@ -17,10 +18,12 @@ public class Patient {
 	@NotBlank(message = "LastName is mandatory")
 	private String lastName;
 	@NotBlank(message = "Birthday is mandatory")
+	@Pattern(regexp = "^\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01])$", message = "The date must be in the format YYYY-MM-DD, ex : 1966-12-31")
 	private String birthday;
 	@NotBlank(message = "Gender is mandatory")
 	private String gender;
 	private String address;
+	@Pattern(regexp = "^\\d{3}-\\d{3}-\\d{4}$", message = "The phone number must be in the format XXX-XXX-XXXX, ex : 100-222-3333")
 	private String phone;
 	
 	public Integer getId() {
